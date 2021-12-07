cb5293cb4e29bf37be1b0d66f7f4d83c3ca350b5
Adam Ploof <adamploof@hotmail.com>
2021-12-06 22:51:14 -0500

    made palette editor expand/collapsible

--- PaletteBlender/FrontEnd/assets/js/components/paletteInterface/PaletteEditor.jsx
+++ PaletteBlender/FrontEnd/assets/js/components/paletteInterface/PaletteEditor.jsx
@@ -1,7 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import iro from '@jaames/iro';
 
 function PaletteEditor() {
+    const [visibility, setVisibility] = useState('hide');
+
+    const getBodyClass = () => {
+        let bodyClass = "editor-body";
+        if (visibility === 'hide') {
+            bodyClass += ' collapse';
+        } else {
+            bodyClass += ' expanded';
+        }
+
+        return bodyClass;
+    };
+
+    const expandPaletteEditor = () => {
+        const content = document.getElementsByClassName('content')[0];
+
+        // If current visibility is 'hide' then we are about to expand the editor -- add margin.
+        if (visibility === 'hide') {
+            content.style.marginBottom = "195px";
+        } else {
+            content.style.marginBottom = "0";
+        }
+
+        setVisibility(visibility === 'hide' ? 'show' : 'hide');
+    };
+
     return (
         <div className="footer-container">
             <div id="palette-editor">
@@ -13,12 +39,16 @@ function PaletteEditor() {
                         </div>
                     </div>
                     <div className="header-right">
-                        <div className="btn btn-sm btn-editor-control">
-                            Hide
+                        <div 
+                            className="btn btn-sm btn-editor-control"
+                            tabIndex="1"
+                            onClick={expandPaletteEditor}
+                        >
+                            {visibility == 'hide' ? 'Show' : 'Hide'}
                         </div>
                     </div>
                 </div>
-                <div className="editor-body">
+                <div className={getBodyClass()}>
                     <div className="editor-section">
                         <div className="palette-selector">
 
