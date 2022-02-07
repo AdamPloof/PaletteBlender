e53837f7093d1f58f222155038e744bba368dccf
Adam Ploof <adamploof@hotmail.com>
2022-02-07 17:40:58 -0500

    edit hsl values for selected palette, initial functionality

--- PaletteBlender/FrontEnd/assets/js/components/paletteInterface/ShadeEditor.jsx
+++ PaletteBlender/FrontEnd/assets/js/components/paletteInterface/ShadeEditor.jsx
@@ -3,6 +3,8 @@ import iro from '@jaames/iro';
 
 import { PaletteContext } from "../PaletteContext";
 
+const MAX_HSL_VALUES = {h: 360, s: 100, l: 100};
+
 // Take the average hue and saturation for the selected palette
 // Only count colors that aren't locked.
 function getAverageHueAndSaturation(palette) {
@@ -31,27 +33,54 @@ function getAverageHueAndSaturation(palette) {
     return [Math.round(avgHue), Math.round(avgSaturation), Math.round(avgLightness)];
 }
 
+// Create an iro color object from the current color, update the hsl property with the new value and convert back to hex string
+function adjustColorHSL(color, baseColor, hslProp, percentChange) {
+    const iroColor = new iro.Color(color.color);
+    const hsl = {...iroColor.hsl};
+    
+    // Set the hsl property back to the starting point for the property being edited so that we change it relative to its starting value
+    iroColor.set(baseColor.color);
+    hsl[hslProp] = iroColor.hsl[hslProp];
+
+    if (percentChange > 0) {
+        hsl[hslProp] = Math.round(hsl[hslProp] + ((MAX_HSL_VALUES[hslProp] - hsl[hslProp]) * percentChange));
+    } else {
+        hsl[hslProp] = Math.round(hsl[hslProp] + (hsl[hslProp] * percentChange));
+    }
+
+    iroColor.set(hsl);
+
+    const updatedColor = {...color};
+    updatedColor.color = iroColor.hexString;
+
+    return updatedColor;
+};
+
 export default function ShadeEditor(props) {
     const [colorPalette, setColorPalette] = useContext(PaletteContext);
-    const selectedPalette = colorPalette[props.selectedPaletteName];
 
-    const [avgHue, avgSat, avgLight] = getAverageHueAndSaturation(selectedPalette);
+    const [userHasModifiedHSL, setUserHasModifiedHSL] = useState(false);
+    const [initialPalette, setInitialPalette] = useState(JSON.parse(JSON.stringify(colorPalette[props.selectedPaletteName])));
+    const [startingHSL, setStartingHSL] = useState({h: 0, s: 0, l: 0});
 
-    const [preventSliderReset, setPreventSliderReset] = useState(false);
+    const [avgHue, avgSat, avgLight] = getAverageHueAndSaturation(colorPalette[props.selectedPaletteName]);
     const [hue, setHue] = useState(avgHue);
     const [saturation, setSaturation] = useState(avgSat);
     const [lightness, setLightness] = useState(avgLight);
 
     useEffect(() => {
         setSlidersToAverageHSL();
-        setPreventSliderReset(false);
+        setUserHasModifiedHSL(false);
+        setInitialPalette(JSON.parse(JSON.stringify(colorPalette[props.selectedPaletteName])));
     }, [props.selectedPaletteName]);
 
     // TODO: In order to prevent unnecessary rerender, infinite loops, etc. We should only update the sliders to avg when either:
-    // 1. The selected sub palette changed, 2. a locked color status changed, 3. a color was reset, 4. entire sub palette reset
+    // 2. a locked color status changed, 3. a color was reset, 4. entire sub palette reset
+    // Most important is to update sliders avg on sub palette reset. The rest is probably overkill.
     useEffect(() => {
-        if (!preventSliderReset) {
+        if (!userHasModifiedHSL) {
             setSlidersToAverageHSL();
+            setInitialPalette(JSON.parse(JSON.stringify(colorPalette[props.selectedPaletteName])));
         }
     }, [colorPalette]);
 
@@ -60,7 +89,63 @@ export default function ShadeEditor(props) {
         setHue(avgHue);
         setSaturation(avgSat);
         setLightness(avgLight);
-    }
+
+        setStartingHSL({
+            h: avgHue,
+            s: avgSat,
+            l: avgLight,
+        });
+    };
+
+    const beginUserEditing = () => {
+        if (userHasModifiedHSL) {
+            return;
+        }
+
+        setUserHasModifiedHSL(true);
+        setInitialPalette(JSON.parse(JSON.stringify(colorPalette[props.selectedPaletteName])));
+    };
+
+    const adjustPaletteHSL = (e) => {
+        // Get HSL val for each color in palette
+        let hslProp;
+        switch (e.target.id) {
+            case 'hue':
+                hslProp = 'h';
+                break;
+            case 'saturation':
+                hslProp = 's';
+                break;
+            case 'lightness':
+                hslProp = 'l';
+                break;
+            default:
+                console.log('HSL slider not adjusted!');
+        }
+
+        let percentChange;
+        if (e.target.value > startingHSL[hslProp]) {
+            // Value has increased
+            percentChange = (e.target.value - startingHSL[hslProp]) / (MAX_HSL_VALUES[hslProp] - startingHSL[hslProp]);
+        } else {
+            // Value has decreased
+            percentChange = ((startingHSL[hslProp] - e.target.value) / startingHSL[hslProp]) * -1;
+        }
+
+        const updatedSubpalette = colorPalette[props.selectedPaletteName].map(color => {
+            if (color.locked === true) {
+                return color;
+            }
+
+            const baseColor = initialPalette.find(c => c.name === color.name);
+            return adjustColorHSL(color, baseColor, hslProp, percentChange);
+        });
+        
+        const updatedPalette = {...colorPalette};
+        updatedPalette[props.selectedPaletteName] = updatedSubpalette;
+
+        setColorPalette(updatedPalette);
+    };
 
     return (
         <div className="shade-editor">
@@ -80,8 +165,9 @@ export default function ShadeEditor(props) {
                                 max="360"
                                 value={hue}
                                 onChange={(e) => {
-                                    setPreventSliderReset(true);
+                                    beginUserEditing();
                                     setHue(e.target.value)
+                                    adjustPaletteHSL(e);
                                 }}
                             />
                         </div>
@@ -102,8 +188,9 @@ export default function ShadeEditor(props) {
                                 max="100"
                                 value={saturation}
                                 onChange={(e) => {
-                                    setPreventSliderReset(true);
+                                    beginUserEditing();
                                     setSaturation(e.target.value)
+                                    adjustPaletteHSL(e);
                                 }}
                             />
                         </div>
@@ -124,8 +211,9 @@ export default function ShadeEditor(props) {
                                 max="100"
                                 value={lightness}
                                 onChange={(e) => {
-                                    setPreventSliderReset(true);
+                                    beginUserEditing();
                                     setLightness(e.target.value)
+                                    adjustPaletteHSL(e);
                                 }}
                             />
                         </div>
