e4bf48012bece480d83b114b6a6f8961eab97387
Adam Ploof <adamploof@hotmail.com>
2022-02-13 23:34:31 -0500

    initial dynamic styles for sidebar container and cards

--- PaletteBlender/FrontEnd/assets/js/components/content/Sidebar.jsx
+++ PaletteBlender/FrontEnd/assets/js/components/content/Sidebar.jsx
@@ -1,83 +1,70 @@
 import React from "react";
+import { createUseStyles } from 'react-jss';
 import { generateUID } from "../uidGenerator";
 
-const rainbowListItems = [
-    {
-        title: 'Relevant Info Report',
-        details: 'T. Fergusson - 172 Views',
-        logoClass: 'danger',
-    },
-    {
-        title: 'Goodbye Presentation',
-        details: 'Emmanuel Lasker - 12k Views',
-        logoClass: 'success',
-    },
-    {
-        title: 'Tour Dates by City',
-        details: 'David St. Nubbins - 5 Views',
-        logoClass: 'primary',
-    },
-    {
-        title: 'Marching Band Expenses',
-        details: 'B.D. Tubatone - 245 Views',
-        logoClass: 'warning',
-    },
-    {
-        title: 'Collect Call Policy',
-        details: 'AT-5000 Auto-Dialer - 17m Views',
-        logoClass: 'danger',
-    },
-    {
-        title: 'Old Timey Maps',
-        details: 'Daniel Kern - 1.5k Views',
-        logoClass: 'primary',
-    },
-    {
-        title: 'Figments of Imagination',
-        details: 'Tracy Waterville - 458 Views',
-        logoClass: 'success',
-    },
-    {
-        title: 'Get Outdoors Study',
-        details: 'Tina - 12b Views',
-        logoClass: 'success',
-    },
-    {
-        title: 'Next Tuesday',
-        details: 'Wednesday Topper - 11 Views',
-        logoClass: 'warning',
-    },
-];
+const baseLogoStyle = {
+    borderRadius: '50%',
+    textAlign: 'center',
+    padding: '.5rem',
+    minHeight: '20px',
+    minWidth: '20px',
+    lineHeight: '20px',
+    alignSelf: 'center',
+};
 
-const primaryListItems = [
-    {
-        title: 'Solar Eclipse BBBQ',
-        details: "The extra 'b' is for BYOBB. What's that extra...",
-        logoClass: 'primary-dark',
+const usesidebarClasses = createUseStyles({
+    sidebarHeader: {
+        backgroundColor: (colorPalette) => colorPalette.primary[2].color,
+        color: (colorPalette) => colorPalette.greys[8].color,
     },
-    {
-        title: 'Collective Cooperation Conference',
-        details: 'Register in advance to be entered into a raffle to win...',
-        logoClass: 'primary-dark',
+    sidebarInput : {
+        "&:active": {
+            borderColor: (colorPalette) => colorPalette.primary[5].color,
+        },
+        "&:focus": {
+            borderColor: (colorPalette) => colorPalette.primary[5].color,
+            // TODO: Figure out why this doesn't update on palette changes!
+            boxShadow: (colorPalette) => "0 0 0 0.25rem " + colorPalette.primary[6].color,
+        }
     },
-    {
-        title: 'WoM OT PA FWIW Meeting',
-        details: 'Little of significance will be discussed, attendance mandatory...',
-        logoClass: 'primary-dark',
+    logoPrimaryDark: {
+        backgroundColor: (colorPalette) => colorPalette.primary[2].color,
+        color: (colorPalette) => colorPalette.greys[8].color,
+        ...baseLogoStyle,        
     },
-    {
-        title: 'Color Palette Creation Workshop',
-        details: 'No longer needed thanks to an awesome new...',
-        logoClass: 'primary-dark',
+    logoPrimary: {
+        backgroundColor: (colorPalette) => colorPalette.primary[3].color,
+        color: (colorPalette) => colorPalette.greys[8].color,
+        ...baseLogoStyle,
     },
-];
+    logoSuccess: {
+        backgroundColor: (colorPalette) => colorPalette.success[2].color,
+        color: (colorPalette) => colorPalette.greys[8].color,
+        ...baseLogoStyle,
+    },
+    logoWarning: {
+        backgroundColor: (colorPalette) => colorPalette.warning[2].color,
+        color: (colorPalette) => colorPalette.greys[8].color,
+        ...baseLogoStyle,
+    },
+    logoInfo: {
+        backgroundColor: (colorPalette) => colorPalette.info[3].color,
+        color: (colorPalette) => colorPalette.greys[8].color,
+        ...baseLogoStyle,
+    },
+    logoDanger: {
+        backgroundColor: (colorPalette) => colorPalette.danger[2].color,
+        color: (colorPalette) => colorPalette.greys[8].color,
+        ...baseLogoStyle,
+    },
+});
 
 const makeListItem = (title, details, logoClass) => {
     const uid = generateUID();
 
     return (
         <div key={uid} className="list-item">
-            <div className={"list-item-logo logo-" + logoClass}></div>
+            <div className={logoClass}>&amp;</div>
             <div className="list-item-details">
                 <div className="detail-title">
                     {title}
@@ -91,11 +78,84 @@ const makeListItem = (title, details, logoClass) => {
 }
 
 function Sidebar(props) {
+    const sidebarClasses = usesidebarClasses(props.colorPalette);
+
+    const rainbowListItems = [
+        {
+            title: 'Relevant Info Report',
+            details: 'T. Fergusson - 172 Views',
+            logoClass: sidebarClasses.logoPrimary,
+        },
+        {
+            title: 'Goodbye Presentation',
+            details: 'Emmanuel Lasker - 12k Views',
+            logoClass: sidebarClasses.logoSuccess,
+        },
+        {
+            title: 'Tour Dates by City',
+            details: 'David St. Nubbins - 5 Views',
+            logoClass: sidebarClasses.logoPrimary,
+        },
+        {
+            title: 'Marching Band Expenses',
+            details: 'B.D. Tubatone - 245 Views',
+            logoClass: sidebarClasses.logoWarning,
+        },
+        {
+            title: 'Collect Call Policy',
+            details: 'AT-5000 Auto-Dialer - 17m Views',
+            logoClass: sidebarClasses.logoInfo,
+        },
+        {
+            title: 'Old Timey Maps',
+            details: 'Daniel Kern - 1.5k Views',
+            logoClass: sidebarClasses.logoDanger,
+        },
+        {
+            title: 'Figments of Imagination',
+            details: 'Tracy Waterville - 458 Views',
+            logoClass: sidebarClasses.logoDanger,
+        },
+        {
+            title: 'Get Outdoors Study',
+            details: 'Tina - 12b Views',
+            logoClass: sidebarClasses.logoSuccess,
+        },
+        {
+            title: 'Next Tuesday',
+            details: 'Wednesday Topper - 11 Views',
+            logoClass: sidebarClasses.logoPrimary,
+        },
+    ];
+    
+    const primaryListItems = [
+        {
+            title: 'Solar Eclipse BBBQ',
+            details: "The extra 'b' is for BYOBB. What's that extra...",
+            logoClass: sidebarClasses.logoPrimaryDark,
+        },
+        {
+            title: 'Collective Cooperation Conference',
+            details: 'Register in advance to be entered into a raffle to win...',
+            logoClass: sidebarClasses.logoPrimaryDark,
+        },
+        {
+            title: 'WoM OT PA FWIW Meeting',
+            details: 'Little of significance will be discussed, attendance mandatory...',
+            logoClass: sidebarClasses.logoPrimaryDark,
+        },
+        {
+            title: 'Color Palette Creation Workshop',
+            details: 'No longer needed thanks to an awesome new...',
+            logoClass: sidebarClasses.logoPrimaryDark,
+        },
+    ];
+
     return (
         <div className="sidebar-container">
             {/* Primary-dark cards */}
             <div className="sidebar-card">
-                <div className="card-header">
+                <div className={sidebarClasses.sidebarHeader + " card-header"}>
                     <div className="card-logo">&amp;</div>
                     <div className="card-title"><h3>UPCOMMING EVENTS</h3></div>
                 </div>
@@ -108,13 +168,13 @@ function Sidebar(props) {
 
             {/* Rainbow Cards */}
             <div className="sidebar-card">
-                <div className="card-header">
+                <div className={sidebarClasses.sidebarHeader + " card-header"}>
                     <div className="card-logo">&amp;</div>
                     <div className="card-title"><h3>FARCICAL READING</h3></div>
                 </div>
                 <div className="card-toolbar">
                     <div className="toolbar-input">
-                        <input type="text" placeholder="Search for stuff..." onClick={(e) => {e.preventDefault()}} />
+                        <input class={sidebarClasses.sidebarInput} type="text" placeholder="Search for stuff..." onClick={(e) => {e.preventDefault()}} />
                     </div>
                 </div>
                 <div className="card-body">
