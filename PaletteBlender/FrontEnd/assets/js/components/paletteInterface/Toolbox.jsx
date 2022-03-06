6a056d64afd1da7cc1e37b40e5304eeaf44076f2
Adam Ploof <adamploof@hotmail.com>
2022-03-06 16:19:16 -0500

    output color palette as css in css toolbox modal

--- PaletteBlender/FrontEnd/assets/js/components/paletteInterface/Toolbox.jsx
+++ PaletteBlender/FrontEnd/assets/js/components/paletteInterface/Toolbox.jsx
@@ -1,12 +1,62 @@
-import React, { useEffect, useRef } from "react";
+import React, { useContext } from "react";
+import { PaletteContext } from "../PaletteContext";
+
+function formatColorName(colorName) {
+    let formattedColorName = '';
+    for (let i = 0; i < colorName.length; i++){
+        if (colorName[i] === colorName[i].toUpperCase()) {
+            formattedColorName += ('-' + colorName[i].toLowerCase());
+        } else {
+            formattedColorName += colorName[i];
+        }
+    }
+
+    return formattedColorName;
+}
+
+function convertPaletteToCSS(colorPalette) {
+    let css = ':root {\n';
+    for (const [ subPaletteName, colors] of Object.entries(colorPalette)) {
+        css += `\t// ${subPaletteName}\n`;
+        for (const color of colors) {
+            css += `\t--${formatColorName(color.name)}: ${color.color};`;
+
+            if (color != color[colors.length - 1]) {
+                css += '\n';
+            }
+        }
+
+        if (subPaletteName !== Object.keys(colorPalette)[Object.keys(colorPalette).length - 1]) {
+            css += '\n';
+        }
+    }
+    css += '}\n';
+
+    return css;
+}
 
 export default function Toolbox(props) {
+    const [ colorPalette ] = useContext(PaletteContext);
+
     const hideToolbox = () => {
         props.toolboxModal.current.classList.remove('show');
 
         if (!props.toolboxModal.current.classList.contains('hide')) {
             props.toolboxModal.current.classList.add('hide');
         }
+    };
+
+    const getOutputSheet = () => {
+        return (
+            <div className="stylesheet-output">
+                <pre>
+                    <code>
+                        {convertPaletteToCSS(colorPalette)}
+                    </code>
+                </pre>
+
+            </div>
+        );
     };
 
     return (
@@ -18,7 +68,7 @@ export default function Toolbox(props) {
                         <span className="modal-close" onClick={hideToolbox}>&times;</span>
                     </div>
                     <div className="modal-body">
-                        <p>Modal test</p>
+                        {getOutputSheet()}
                     </div>
                     <div className="modal-footer">
                         <div className="btn btn-dark" onClick={hideToolbox}>Close</div>
