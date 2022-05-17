a9f0204a516233fe5e97478535d5fbe5a108e47b
Adam Ploof <adamploof@hotmail.com>
2022-05-16 22:16:56 -0400

    added static root directory and updated path to media root in production settings

--- PaletteBlender/PaletteBlender/settings_prod.py
+++ PaletteBlender/PaletteBlender/settings_prod.py
@@ -129,14 +129,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
 # Media
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
